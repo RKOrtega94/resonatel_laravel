a5c74c83e492c13ff1e248bc44af274024eac0ac
RKOrtega <rkortega1994@gmail.com>
2019-12-28 18:33:18 -0500

    Fix error page

--- resources/views/layouts/footers/auth.blade.php
+++ resources/views/layouts/footers/auth.blade.php
@@ -1,11 +1,13 @@
-<footer class="footer">
+<footer class="footer" style="padding: 0%">
     <div class="container-fluid">
-        <div class="copyright float-right">
+        <div class="copyright float-right" style="margin: 0%; padding: 0%">
             &copy;
             <script>
                 document.write(new Date().getFullYear())
             </script>, made with <i class="material-icons">favorite</i> by
             <a href="https://www.linkedin.com/in/robinson-ortega-mafla-8686b9197" target="_blank">Ortega Robinson</a>.
+            <img src="{{ asset('images/icon.ico') }}" alt="RESONATEL" class="icon-bar navbar-brand margin-0"
+                height="40" style="margin: 0em">
         </div>
     </div>
 </footer>
