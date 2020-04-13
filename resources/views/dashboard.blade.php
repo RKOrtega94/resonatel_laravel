03232c35c474289e524958028a935e52a8db8240
RKOrtega <rkortega1994@gmail.com>
2020-04-12 23:29:49 -0500

    adding messaje

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,22 +1,6 @@
 @extends('layouts.app', ['activePage' => 'home', 'titlePage' => __(' - Home')])
 @section('content')
 <div class="content">
-    <div class="container-fluid">
-        <div class="row">
-            @include('helpers.passwordalert')
-        </div>
-        <div class="container-fluid justify-content-end">
-            @php
-            $userLogin = App\User::userLogin(Auth::user()->id);
-            $pos = strrpos($userLogin->firstName, ' ');
-            if($userLogin->profile === 'Asesor'){
-            echo "<p class='font-weight-bold text-dark align-content-end'>Hola, ".substr($userLogin->firstName, 0,
-                $pos)."<br> El sol no es suficiente para iluminar todo el planeta, <em>¡También te necesita a ti!</em>
-            </p>";
-            }
-            @endphp
-        </div>
-    </div>
 </div>
 @endsection
 
